e06dd541b7c1e17f2f96fc1a114235d56ecf541d
@Rakesh953 <rakeshmeher953@gmail.com>
2023-11-10 14:09:51 +0530

    first commit

---
+++ app4/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def hrs1(request):
+    return render(request,'h1.html')
+
+def hrs2(request):
+    return render(request,'h2.html')
+
+def hrs3(request):
+    return render(request,'h3.html')
+
+def hrs4(request):
+    return render(request,'h4.html')
+# Create your views here.
